b196bf7912fe341894a7770db1b491da2749b801
LYFhub <liyf963@163.com>
2017-05-04 14:28:26 +0800

    add chart file and draw chart

---
+++ js/chart.js
@@ -0,0 +1,76 @@
+// 声明函数
+function drawChart(){
+	// 基于准备好的dom，初始化echarts实例
+    var barChart = echarts.init($('.barchart')[0]);
+    var pieChart = echarts.init($('.piechart')[0]);
+    var lineChart = echarts.init($('.linechart')[0]);
+
+    // 指定图表的配置项和数据
+    var barOption = {
+        title: {
+            text: '资源'
+        },
+        tooltip: {},
+        legend: {
+            data:['销量']
+        },	
+        xAxis: {
+            data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+        },
+        yAxis: {
+        	text: '天数'
+        },
+        series: [{
+            name: '销量',
+            type: 'bar',
+            data: [5, 20, 36, 10, 10, 20]
+        }]
+    };
+
+    // 使用刚指定的配置项和数据显示图表。
+    barChart.setOption(barOption);
+
+    // 指定图表的配置项和数据
+    var pieOption = {
+        title: {
+            text: '成本'
+        },
+        tooltip: {},
+        legend: {
+            data:['销量']
+        },
+        series: [{
+            name: '销量',
+            type: 'pie',
+            data: [5, 20, 36, 10, 10, 20]
+        }]
+    };
+
+    // 使用刚指定的配置项和数据显示图表。
+    pieChart.setOption(pieOption);
+
+    // 指定图表的配置项和数据
+    var lineOption = {
+        title: {
+            text: '进度'
+        },
+        tooltip: {},
+        legend: {
+            data:['销量']
+        },	
+        xAxis: {
+            data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+        },
+        yAxis: {
+        	text: '天数'
+        },
+        series: [{
+            name: '销量',
+            type: 'line',
+            data: [5, 20, 36, 10, 10, 20]
+        }]
+    };
+
+    // 使用刚指定的配置项和数据显示图表。
+    lineChart.setOption(lineOption);
+}
---
+++ js/main.js
@@ -0,0 +1,16 @@
+// 取得所有的tab 和 tab content
+var allTabsHeader = $('.topcontent ul.nav-tabs li a');
+var allTabContent = $('.topcontent div.tabcontent');
+
+// 当点击某一个tab时，判断出当前点击tab的序号，然后将对应序号的tab content 设置为显示，其他的隐藏
+function changetab(event) {
+	for (var i = 0, j = allTabsHeader.length; i < j; i++) {
+		if (event.target.className === allTabsHeader[i].className){
+			for(k = 0,num = allTabContent.length; k < num; k++) {
+				$(allTabContent[k]).css('display','none');
+			}
+			$(allTabContent[i]).css('display','block');
+			drawChart();// 先显示，然后再画图，否则尺寸会有问题
+		}
+	}
+}
