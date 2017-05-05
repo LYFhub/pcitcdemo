9666fd1d4365871aa666aa5797c931d5a79ddb36
LYFhub <liyf963@163.com>
2017-05-05 17:58:40 +0800

    add project timeline

--- js/calendar.js
+++ js/calendar.js
@@ -26,57 +26,57 @@ $(document).ready(function() {
 		eventLimit: true, // allow "more" link when too many events
 		events: [
 			{
-				title: 'All Day Event',
+				title: '项目A',
 				start: '2017-05-01'
 			},
 			{
-				title: 'Long Event',
+				title: '项目A',
 				start: '2017-05-07',
 				end: '2017-05-10'
 			},
 			{
 				id: 999,
-				title: 'Repeating Event',
+				title: '项目A',
 				start: '2017-05-09T16:00:00'
 			},
 			{
 				id: 999,
-				title: 'Repeating Event',
+				title: '项目A',
 				start: '2017-05-16T16:00:00'
 			},
 			{
-				title: 'Conference',
+				title: '项目B',
 				start: '2017-05-11',
 				end: '2017-05-13'
 			},
 			{
-				title: 'Meeting',
+				title: '项目B',
 				start: '2017-05-12T10:30:00',
 				end: '2017-05-12T12:30:00'
 			},
 			{
-				title: 'Lunch',
+				title: '项目B',
 				start: '2017-05-12T12:00:00'
 			},
 			{
-				title: 'Meeting',
+				title: '项目C',
 				start: '2017-05-12T14:30:00'
 			},
 			{
-				title: 'Happy Hour',
+				title: '项目C',
 				start: '2017-05-12T17:30:00'
 			},
 			{
-				title: 'Dinner',
+				title: '项目C',
 				start: '2017-05-12T20:00:00'
 			},
 			{
-				title: 'Birthday Party',
+				title: '项目C',
 				start: '2017-05-13T07:00:00'
 			},
 			{
-				title: 'Click for Google',
-				url: 'http://google.com/',
+				title: '项目C',
+				url: 'http://baidu.com/',
 				start: '2017-05-28'
 			}
 		]
